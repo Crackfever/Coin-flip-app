518a8d0b50c43aacc41772a66998d63f84f0e3ef
Crackfever <b1s2p3@gmail.com>
2020-05-02 09:00:55 +0000

    Added user input

---
+++ main.py
@@ -0,0 +1,11 @@
+#Coin flip app
+print("Welcome to the Coin Flip App")
+print("I will flip a coin a set number of times.")
+
+#User input
+flip_number = int(input("How many times would you like to flip the coin: "))
+choice = input("Would you like to see the result of each flip (y/n): ").lower()
+
+print("\nFlipping!!!\n")
+
+
