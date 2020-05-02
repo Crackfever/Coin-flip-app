d36c78ca9524d0f733f37aca6c77467c1e226272
Crackfever <b1s2p3@gmail.com>
2020-05-02 09:29:13 +0000

    Added results and finishing touches

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 
 print("Welcome to the Coin Flip App")
-print("I will flip a coin a set number of times.")
+print("\nI will flip a coin a set number of times")
 
 #User input
 flip_number = int(input("How many times would you like to flip the coin: "))
@@ -25,9 +25,20 @@ for flips in range(flip_number):
       print("HEADS")
   else:
     tails += 1
-    if choice,startswith("y"):
+    if choice.startswith("y"):
       print("TAILS")
 
   if heads == tails:
-    print("At " + str(flips + 1) + " flips, the number of heads and tails were equal at " + str(heads)
+    print("At " + str(flips + 1) + " flips, the number of heads and tails were equal at " + str(heads) + " each.")
+#Calculate
+heads_percentage = round(100*heads/flip_number, 2)
+tails_percentage = round(100*heads/flip_number, 2)
+
+#Print results
+print("\nResults of flipping a coin " + str(flip_number) + " Times: ")
+print("\nSide\t\tCount\t\tPercentage")
+print("Heads\t\t" + str(heads) + "/" + str(flip_number) + "\t\t" + str(heads_percentage) + "%")
+print("Tails\t\t" + str(tails) + "/" + str(flip_number) + "\t\t" + str(tails_percentage) + "%")
+
+
 
